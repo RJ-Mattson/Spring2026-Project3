72fed45eb538e281b9520353758507ced3634c3d
RJ Mattson <rjmatts@gmail.com>
2026-04-08 19:29:46 -0500

    slight progress

--- Spring2026-Project3-RJmattson/Data/Migrations/20260409002742_InitialDomainModels.Designer.cs
+++ Spring2026-Project3-RJmattson/Data/Migrations/20260409002742_InitialDomainModels.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Spring2026_Project3_RJmattson.Data;
 
@@ -11,9 +12,11 @@ using Spring2026_Project3_RJmattson.Data;
 namespace Spring2026_Project3_RJmattson.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260409002742_InitialDomainModels")]
+    partial class InitialDomainModels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
