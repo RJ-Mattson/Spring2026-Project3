198efcaacceb7d2176c6f44f00713b6e00628ec3
RJ Mattson <rjmatts@gmail.com>
2026-04-12 23:25:41 -0500

    New base. Screwed up some logic so had to revert

--- Spring2026-Project3-RJmattson/Data/ApplicationDbContext.cs
+++ Spring2026-Project3-RJmattson/Data/ApplicationDbContext.cs
@@ -9,8 +9,28 @@ namespace Spring2026_Project3_RJmattson.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
-    public DbSet<Actor> Actors { get; set; }
+
+        public DbSet<Actor> Actors { get; set; }
         public DbSet<Movie> Movies { get; set; }
         public DbSet<MovieActorRel> ActorMovies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<MovieActorRel>()
+                .HasIndex(ma => new { ma.ActorId, ma.MovieId })
+                .IsUnique();
+            modelBuilder.Entity<MovieActorRel>()
+                .HasOne(ma => ma.Movie)
+                .WithMany(m => m.ActorMovies)
+                .HasForeignKey(ma => ma.MovieId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<MovieActorRel>()
+                .HasOne(ma => ma.Actor)
+                .WithMany(a => a.ActorMovies)
+                .HasForeignKey(ma => ma.ActorId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
--- Spring2026-Project3-RJmattson/Models/Actor.cs
+++ Spring2026-Project3-RJmattson/Models/Actor.cs
@@ -10,7 +10,7 @@
         public byte[]? Poster { get; set; }
 
 
-        public virtual ICollection<MovieActorRel> MovieActorRels { get; set; }
+        public virtual ICollection<MovieActorRel> ActorMovies { get; set; } = new List<MovieActorRel>();
 
 
     }
